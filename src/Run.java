fa8f41479b60da896836cf22a70a2ff073c3b270
stockfishdeveloper <joeblow@gmail.com>
2016-06-15 13:20:29 -0700

    Code used to test the library
    Temporary code to test the library as I go along

--- src/Run.java
+++ src/Run.java
@@ -3,13 +3,15 @@ class Run
 	public static void main(String[] args)
 	{
 		World world = new World();
-		for(int i = 0; i < 300; i++)
+		/*for(int i = 0; i < 300; i++)
 		{
-			world.s.RotateClockwiseAboutXAxis(1f);
-			world.s.TranslateVisiblePosition(1, 1);
-			world.s.RotateClockwiseAboutZAxis(1f);
-			world.s.TranslateVisiblePosition(1, 1);
-			world.s.RotateClockwiseAboutYAxis(1f);
-		}
+			world.c.RotateClockwiseAboutXAxis(1f);
+			world.c.TranslateVisiblePosition(1, 1);
+			world.c.RotateClockwiseAboutZAxis(1f);
+			world.c.TranslateVisiblePosition(1, 1);
+			world.c.RotateClockwiseAboutYAxis(1f);
+		}*/
+		//world.c.RotateClockwiseAboutYAxis(90);
+		//world.c.paintImmediately(0, 0, 1280, 1000);
 	}
 }
