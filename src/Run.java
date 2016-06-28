d9b0e85ce681eb3406fc2135a54badabcebf0514
stockfishdeveloper <joeblow@gmail.com>
2016-06-28 13:15:55 -0500

    Make a cube of Cubes
    Animate 3 layers of 9 Cubes to make a 3d cube of Cubes

--- src/Run.java
+++ src/Run.java
@@ -3,25 +3,41 @@ class Run
 	public static void main(String[] args)
 	{
 		World world = new World();
-		/*for(float i = 1; i < 2; i += 0.3)
+		/*for(int i = 0; i < 1000; i++)
 		{
-			world.t.Resize(i);
+			world.t.Resize(1.001f);
 		}*/
 		/*for(int i = 0; i < 360; i++)
 		{
-			world.c.RotateClockwiseAboutXAxis(1f);
-			world.c.TranslateVisiblePosition(1, 1);
-			world.c.RotateClockwiseAboutZAxis(1f);
-			world.c.TranslateVisiblePosition(1, 1);
-			world.c.RotateClockwiseAboutYAxis(1f);
+			world.t.RotateClockwiseAboutXAxis(1f);
+			world.t.TranslateVisiblePosition(1, 1);
+			world.t.RotateClockwiseAboutZAxis(1f);
+			world.t.TranslateVisiblePosition(1, 1);
+			world.t.RotateClockwiseAboutYAxis(1f);
 		}*/
-		/*for(int i = 0; i < 360; i++)
+		/*for(int i = 0; i < 10000; i++)
 		{
-			world.c.RotateClockwiseAboutYAxis(1f);
+			world.t.RotateClockwiseAboutYAxis(1f);
+			world.t.RotateClockwiseAboutXAxis(1f);
+			world.t.RotateClockwiseAboutZAxis(1f);
+			world.t.RotateCounterClockwiseAboutZAxis(1f);
+			world.t.RotateCounterClockwiseAboutYAxis(1f);
+			world.t.RotateCounterClockwiseAboutXAxis(1f);
 		}*/
-		/*for(int i = 0; i < 360; i++)
+		for(int i = 0; i < 360; i++)
 		{
-			world.c.RotateClockwiseAboutZAxis(1f);
+			for(int j = 0; j < 27; j++)
+			{
+			world.t[j].RotateClockwiseAboutYAxis(5f);
+			world.t[j].RotateClockwiseAboutXAxis(5f);
+			}
+		}
+		/*for(int i = 0; i < 1000000; i++)
+		{
+			Util.Distance_Between(new Point(i, i, i), new Point(i, i, i));
+			Util.GetNormalVector(new Point(i, i, i), new Point(i, i, i), new Point(i, i, i));
+			Util.NormalizeVector(new Point(i, i, i));
+			Util.GetDotProduct(new Point(i, i, i), new Point(i, i, i));
 		}*/
 	}
 }
