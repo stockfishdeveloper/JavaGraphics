2c49fea8291b9d7297fd50c5a8c4d3251af55c1e
stockfishdeveloper <davidandjava@mail.com>
2016-07-31 22:45:58 -0500

    Add SameLocation()
    To determine if two points refer to the same world space

--- src/Point.java
+++ src/Point.java
@@ -3,6 +3,12 @@ class Point
 	private double x;
 	private double y;
 	private double z;
+	Point()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+	}
 	Point(int fx, int fy, int fz)
 	{
 		x = (float) fx;
@@ -53,50 +59,68 @@ class Point
 	}
 	void RotateClockwiseAboutYAxis(float degrees)
 	{
-		double hx = z * (Math.sin(-degrees / 57.2958));
-		hx = hx + (x * Math.cos(-degrees / 57.2958));
-		double hz = z * (Math.cos(-degrees / 57.2958));
-		z = hz - (x * Math.sin(-degrees / 57.2958));
+		double s = Math.sin(-degrees / 57.2958);
+		double c = Math.cos(-degrees / 57.2958);
+		double hx = z * s;
+		hx = hx + (x * c);
+		double hz = z * c;
+		z = hz - (x * s);
 		x = hx;
 	}
 	void RotateCounterClockwiseAboutYAxis(float degrees)
 	{
-		double hx = z * (Math.sin(degrees / 57.2958));
-		hx = hx + (x * Math.cos(degrees / 57.2958));
-		double hz = z * (Math.cos(degrees / 57.2958));
-		z = hz - (x * Math.sin(degrees / 57.2958));
+		double s = Math.sin(degrees / 57.2958);
+		double c = Math.cos(degrees / 57.2958);
+		double hx = z * s;
+		hx = hx + (x * c);
+		double hz = z * c;
+		z = hz - (x * s);
 		x = hx;
 	}
 	void RotateClockwiseAboutXAxis(float degrees)
 	{
-		double hy = y * (Math.cos(-degrees / 57.2958));
-		hy = hy - (z * Math.sin(-degrees / 57.2958));
-		double hz = y * (Math.sin(-degrees / 57.2958));
-		z = hz + (z * Math.cos(-degrees / 57.2958));
+		double s = Math.sin(-degrees / 57.2958);
+		double c = Math.cos(-degrees / 57.2958);
+		double hy = y * c;
+		hy = hy - (z * s);
+		double hz = y * s;
+		z = hz + (z * c);
 		y = hy;
 	}
 	void RotateCounterClockwiseAboutXAxis(float degrees)
 	{
-		double hy = y * (Math.cos(degrees / 57.2958));
-		hy = hy - (z * Math.sin(degrees / 57.2958));
-		double hz = y * (Math.sin(degrees / 57.2958));
-		z = hz + (z * Math.cos(degrees / 57.2958));
+		double s = Math.sin(degrees / 57.2958);
+		double c = Math.cos(degrees / 57.2958);
+		double hy = y * c;
+		hy = hy - (z * s);
+		double hz = y * s;
+		z = hz + (z * c);
 		y = hy;
 	}
 	void RotateClockwiseAboutZAxis(float degrees)
 	{
-		double hx = x * (Math.cos(-degrees / 57.2958));
-		hx = hx - (y * Math.sin(-degrees / 57.2958));
-		double hy = x * (Math.sin(-degrees / 57.2958));
-		y = hy + (y * Math.cos(-degrees / 57.2958));
+		double s = Math.sin(-degrees / 57.2958);
+		double c = Math.cos(-degrees / 57.2958);
+		double hx = x * c;
+		hx = hx - (y * s);
+		double hy = x * s;
+		y = hy + (y * c);
 		x = hx;
 	}
 	void RotateCounterClockwiseAboutZAxis(float degrees)
 	{
-		double hx = x * (Math.cos(degrees / 57.2958));
-		hx = hx - (y * Math.sin(degrees / 57.2958));
-		double hy = x * (Math.sin(degrees / 57.2958));
-		y = hy + (y * Math.cos(degrees / 57.2958));
+		double s = Math.sin(degrees / 57.2958);
+		double c = Math.cos(degrees / 57.2958);
+		double hx = x * c;
+		hx = hx - (y * s);
+		double hy = x * s;
+		y = hy + (y * c);
 		x = hx;
+	}
+	public boolean SameLocation(Point p1, Point p2)
+	{
+		if((p1.GetX() == p2.GetX()) && (p1.GetY() == p2.GetY()) && (p1.GetZ() == p2.GetZ()))
+			return true;
+		return false;
 	}
 }
