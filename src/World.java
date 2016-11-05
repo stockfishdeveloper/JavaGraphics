9a0559bbc87f58bfc94951ad5cb6df4869609b4d
stockfishdeveloper <davidandjava@mail.com>
2016-11-05 12:52:52 -0500

    Make World class responsible for game
    Move the actual rendering to the World class and move the setup tasks to the Run class

--- src/World.java
+++ src/World.java
@@ -1,57 +1,79 @@
-import java.awt.event.KeyListener;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-class World extends JComponent implements MouseMotionListener, MouseWheelListener
+class World extends JComponent implements MouseMotionListener, MouseListener, MouseWheelListener
 {
-	final int length = 6;
-	int[] x = new int[length * length * length];
-	int[] y = new int[length * length * length];
-	int[] z = new int[length * length * length];
-	static RectangularPrism[] t = new RectangularPrism[4];
-	//static RectangularPrism c;
+	private static final long serialVersionUID = 1L;
+	RectangularPrism[] t = new RectangularPrism[4];
+	public static ArrayList<Triangle> triangles;
 	int currx = 0;
 	int curry = 0;
-	JFrame frame;
-	static JPanel panel;
-	static Camera camera;
+	public static Camera camera;
+	public SkyBox skybox;
+        
 	public World()
 	{
-	frame = new JFrame();
-	frame.setSize(1280, 1000);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	panel = new JPanel();
-	panel.setLayout(null);
-	t[0] = new RectangularPrism(new Point(0, 0, 2000), 4000, 10, 4000);
-	t[1] = new RectangularPrism(new Point(2000, 0, 0), 10, 4000, 4000);
-	t[2] = new RectangularPrism(new Point(0, 0, -2000), 4000, 10, 4000);
-	t[3] = new RectangularPrism(new Point(-2000, 0, 0), 10, 4000, 4000);
-	//c = new RectangularPrism(new Point(0, 0, 0), 2000, 2000, 10);
-	camera = new Camera();
-	//Triangle tr = new Triangle(new Point(20, 0, 20), new Point(0, 20, 20), new Point(-20, 0, 20));
-	//for(int i = 0; i < 1000; i++)
-	//{
-	//camera.RotateClockwiseAboutYAxis(camera.location, 90);
-	//camera.RotateClockwiseAboutZAxis(camera.location, 45);
-	//camera.MoveForward(1);
-	//}*/
-	//camera.LookAt(tr).Print_Info();
-	//t.TranslateVisiblePosition(600, 500);
-	//panel.addMouseMotionListener(t);
-	panel.addMouseMotionListener(this);
-	panel.addMouseWheelListener(this);
-	//panel.addMouseListener(t);
-	//panel.addMouseWheelListener(t);
-	for(RectangularPrism r : t) panel.add(r);
-	//panel.add(c);
-	frame.add(panel);
-	frame.setVisible(true);
+            t[0] = new RectangularPrism(new Point(0, 0, 2000), 2000, 10, 2000);
+            t[1] = new RectangularPrism(new Point(2000, 0, 0), 10, 2000, 2000);
+            t[2] = new RectangularPrism(new Point(0, 0, -2000), 2000, 10, 2000);
+            t[3] = new RectangularPrism(new Point(-2000, 0, 0), 10, 2000, 2000);
+            /*t[4] = new RectangularPrism(new Point(0, 0, 2000), 2000, 10, 2000);
+            t[5] = new RectangularPrism(new Point(2000, 0, 0), 10, 2000, 2000);
+            t[6] = new RectangularPrism(new Point(0, 0, -2000), 2000, 10, 2000);
+            t[7] = new RectangularPrism(new Point(-2000, 0, 0), 10, 2000, 2000);
+            t[8] = new RectangularPrism(new Point(0, 0, 2000), 2000, 10, 2000);
+            t[9] = new RectangularPrism(new Point(2000, 0, 0), 10, 2000, 2000);
+            t[10] = new RectangularPrism(new Point(0, 0, -2000), 2000, 10, 2000);
+            t[11] = new RectangularPrism(new Point(-2000, 0, 0), 10, 2000, 2000);*/
+            camera = new Camera();
+            triangles = new ArrayList<>();
+            skybox = new SkyBox();
+            skybox.SetFrontFace("C:\\Users\\DesktopHP\\workspace\\3DSquare\\bin\\sky_fr.jpg");
+    		skybox.SetBackFace("C:\\Users\\DesktopHP\\workspace\\3DSquare\\bin\\sky_bk.jpg");
+    		skybox.SetLeftFace("C:\\Users\\DesktopHP\\workspace\\3DSquare\\bin\\sky_lf.jpg");
+    		skybox.SetRightFace("C:\\Users\\DesktopHP\\workspace\\3DSquare\\bin\\sky_rt.jpg");
+    		skybox.SetUpFace("C:\\Users\\DesktopHP\\workspace\\3DSquare\\bin\\sky_up.jpg");
+    		skybox.SetDownFace("C:\\Users\\DesktopHP\\workspace\\3DSquare\\bin\\sky_dn.jpg");
+            this.setBounds(0, 0, 1280, 1000);
+         }
+        @Override
+        public void paintComponent(Graphics gr)
+	{
+		Graphics2D g = (Graphics2D)gr;
+		skybox.Render(g, camera.direction);
+		for(RectangularPrism c : t)
+			c.Render();
+		Collections.sort(triangles);
+		for(Triangle t : triangles)
+		{
+			g.setColor(t.color);
+			int[] x = new int[3];
+			int[] y = new int[3];
+			x[0] = t.points[0].GetX();
+			x[1] = t.points[1].GetX();
+			x[2] = t.points[2].GetX();
+			y[0] = t.points[0].GetY();
+			y[1] = t.points[1].GetY();
+			y[2] = t.points[2].GetY();
+			Polygon poly = new Polygon(x, y, 3);
+			g.fillPolygon(poly);
+		}
+                triangles.clear();
+                camera.Print_Info();
 	}
 	@Override
 	public void mouseDragged(MouseEvent arg0) {
@@ -70,14 +92,40 @@ class World extends JComponent implements MouseMotionListener, MouseWheelListene
 			camera.RotateClockwiseAboutXAxis(camera.location, 0.3f);
 		currx = m.getX();
 		curry = m.getY();
-		
+		repaint();
 	}
 	@Override
 	public void mouseWheelMoved(MouseWheelEvent m) {
 		if(m.getPreciseWheelRotation() < 0)
-			camera.MoveForward(100);
+			camera.MoveForward(200);
 		else
-			camera.MoveBackward(100);	
+			camera.MoveBackward(200);	
+		repaint();
+		
+	}
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 }
