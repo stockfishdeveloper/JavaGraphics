527bd1d2f1779f74ff4569b96602a99d7a8b4c9f
stockfishdeveloper <davidandjava@mail.com>
2016-11-28 15:34:54 -0600

    Create Pixel.java

---
+++ src/Pixel.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+
+class Pixel extends Point
+{
+	Color c;
+	public Pixel(double newx, double newy, double newz, Color newcolor)
+	{
+		this.x = newx;
+		this.y = newy;
+		this.z = newz;
+		this.c = newcolor;
+	}
+	public static Pixel Copy(Pixel other)
+	{
+		return new Pixel(other.x, other.y, other.z, other.c);
+	}
+	public void SetColor(Color newcolor)
+	{
+		c = newcolor;
+	}
+	public void SetColor(int newcolor)
+	{
+		c = new Color(newcolor);
+	}
+	public int GetColor()
+	{
+		return c.getRGB();
+	}
+}
