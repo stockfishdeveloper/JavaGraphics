70caf4107066699ff50a49320c50b6b2fc66360f
stockfishdeveloper <davidandjava@mail.com>
2016-11-29 11:43:48 -0600

    Code cleanup

--- src/RectangularPrism.java
+++ src/RectangularPrism.java
@@ -1,23 +1,23 @@
 import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-@SuppressWarnings("serial")
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 class RectangularPrism
 {
-	int currx = 350;
-	int curry = 350;
-	static Point origin = new Point(350, 350, 0);
 	public int xoffset = 640;
 	public int yoffset = 500;
-	boolean isSelected = false;
-	private Rectangle BoundingBox = new Rectangle();
 	Point[] points = new Point[8];
 	Triangle[] triangles = new Triangle[12];
+	BufferedImage img;
+	Pixel[][][] pixels;
 	public RectangularPrism()
 	{
 		
 	}
-	public RectangularPrism(Point center, int length, int width, int height)
+	public RectangularPrism(Point center, int length, int width, int height, String texture)
 	{
 		for(int i = 0; i < 8; i++) points[i] = new Point(0, 0, 0);
 		points[0].SetX(center.GetExX() - (length / 2));
@@ -43,95 +43,150 @@ class RectangularPrism
 		points[6].SetZ(center.GetExZ() + (width / 2));
 		points[7].SetX(center.GetExX() + (length / 2));
 		points[7].SetY(center.GetExY() - (height / 2));
-		points[7].SetZ(center.GetExZ() + (width / 2));
-		triangles[0] = new Triangle(points[0], points[1], points[2]);
-		triangles[1] = new Triangle(points[1], points[3], points[2]);
-		triangles[2] = new Triangle(points[5], points[4], points[6]);
-		triangles[3] = new Triangle(points[5], points[6], points[7]);
-		triangles[4] = new Triangle(points[4], points[1], points[0]);
-		triangles[5] = new Triangle(points[4], points[5], points[1]);
-		triangles[6] = new Triangle(points[2], points[3], points[7]);
-		triangles[7] = new Triangle(points[2], points[7], points[6]);
-		triangles[8] = new Triangle(points[4], points[0], points[2]);
-		triangles[9] = new Triangle(points[4], points[2], points[6]);
-		triangles[10] = new Triangle(points[1], points[5], points[7]);
-		triangles[11] = new Triangle(points[1], points[7], points[3]);
-	}
-	public void UpdateBoundingBox()
-	{
-		int gx = -10000;
-		int ly = 10000;
-		int lx = 10000;
-		int gy = -10000;
-		for(Triangle t : triangles)
+                points[7].SetZ(center.GetExZ() + (width / 2));
+		try {
+            img = ImageIO.read(new File(texture));
+        } catch (IOException ex) {
+         		}
+		pixels = new Pixel[6][img.getWidth()][img.getHeight()];
+		double unitsperpixel = (double)((length * 2) / (double)(img.getWidth()));
+		//Front
+		for(int i = 0; i < img.getWidth(); i++)
 		{
-			if(t.GreatestX() > gx) gx = t.GreatestX();
-			if(t.LeastX() < lx) lx = t.LeastX();
-			if(t.GreatestY() > gy) gy = t.GreatestY();
-			if(t.LeastY() < ly) ly = t.LeastY();
+			for(int j = 0; j < img.getHeight(); j++)
+			{
+				pixels[0][i][j] = new Pixel((unitsperpixel * (i - (img.getWidth() / 2))), (unitsperpixel * (j - (img.getHeight() / 2))), center.GetExZ() - length, new Color(img.getRGB(i, j)));
+			}
 		}
-		BoundingBox = new Rectangle();
-		BoundingBox.add(gx + xoffset, gy + yoffset);
-		BoundingBox.add(gx + xoffset, ly + yoffset);
-		BoundingBox.add(lx + xoffset, ly + yoffset);
-		BoundingBox.add(lx + xoffset, gy + yoffset);
-		
-	}
-	public void TranslateVisiblePosition(int x, int y)
-	{
-		xoffset += x;
-		yoffset += y;
-		UpdateBoundingBox();
-		
+		//Back
+		for(int i = 0; i < img.getWidth(); i++)
+		{
+			for(int j = 0; j < img.getHeight(); j++)
+			{
+				pixels[1][i][j] = new Pixel((unitsperpixel * (i - (img.getWidth() / 2))), (unitsperpixel * (j - (img.getHeight() / 2))), center.GetExZ() + length, new Color(img.getRGB(i, j)));
+			}
+		}
+		//Top
+		for(int i = 0; i < img.getWidth(); i++)
+		{
+			for(int j = 0; j < img.getHeight(); j++)
+			{
+				pixels[2][i][j] = new Pixel((unitsperpixel * (i - (img.getWidth() / 2))),  center.GetExY() + length, (unitsperpixel * (j - (img.getHeight() / 2))), new Color(img.getRGB(i, j)));
+			}
+		}
+		//Bottom
+		for(int i = 0; i < img.getWidth(); i++)
+		{
+			for(int j = 0; j < img.getHeight(); j++)
+			{
+				pixels[3][i][j] = new Pixel((unitsperpixel * (i - (img.getWidth() / 2))),  center.GetExY() - length, (unitsperpixel * (j - (img.getHeight() / 2))), new Color(img.getRGB(i, j)));
+			}
+		}
+		//Left
+		for(int i = 0; i < img.getWidth(); i++)
+		{
+			for(int j = 0; j < img.getHeight(); j++)
+			{
+				pixels[4][i][j] = new Pixel(center.GetExX() - length, (unitsperpixel *  (j - (img.getWidth() / 2))), (unitsperpixel * (i - (img.getHeight() / 2))), new Color(img.getRGB(i, j)));
+			}
+		}
+		//Right
+		for(int i = 0; i < img.getWidth(); i++)
+		{
+			for(int j = 0; j < img.getHeight(); j++)
+			{
+				pixels[5][i][j] = new Pixel(center.GetExX() + length, (unitsperpixel * (j - (img.getWidth() / 2))), (unitsperpixel * (i - (img.getHeight() / 2))), new Color(img.getRGB(i, j)));
+			}
+		}
+		ArrayList<Pixel> pix = new ArrayList<>();
+		ArrayList<Pixel> pix2 = new ArrayList<>();
+		int inc = img.getWidth();
+		for(int p = 0; p < 6; p++)
+		{
+			for(int i = 0; i < img.getWidth(); i++)
+			{
+				for(int j = 0; j < img.getHeight(); j++)
+				{
+					if(j < inc)
+					{
+						pix.add(pixels[p][i][j]);
+					}
+					else
+					{
+						pix2.add(pixels[p][i][j]);
+					}
+				}
+				inc--;
+			}
+			if(p == 0)
+			{
+				triangles[0] = new Triangle(points[0], points[1], points[2], pix);
+				triangles[1] = new Triangle(points[1], points[3], points[2], pix2);
+			}
+			else if(p == 1)
+			{
+				triangles[2] = new Triangle(points[5], points[4], points[7], pix);
+				triangles[3] = new Triangle(points[4], points[6], points[7], pix2);
+			}
+			else if(p == 2)
+			{
+				triangles[4] = new Triangle(points[4], points[5], points[0], pix);
+				triangles[5] = new Triangle(points[5], points[1], points[0], pix2);
+			}
+			else if(p == 3)
+			{
+				triangles[6] = new Triangle(points[2], points[3], points[6], pix);
+				triangles[7] = new Triangle(points[3], points[7], points[6], pix2);
+			}
+			else if(p == 4)
+			{
+				triangles[8] = new Triangle(points[4], points[0], points[6], pix);
+				triangles[9] = new Triangle(points[0], points[2], points[6], pix2);
+			}
+			else if(p == 5)
+			{
+				triangles[10] = new Triangle(points[1], points[5], points[3], pix);
+				triangles[11] = new Triangle(points[5], points[7], points[3], pix2);
+			}
+			inc = img.getWidth();
+			pix.clear();
+			pix2.clear();
+		}
 	}
 	public void RotateCounterClockwiseAboutYAxis(Point p, float degrees)
 	{
 		for(Triangle t : triangles)
 			t.RotateCounterClockwiseAboutYAxis(p, degrees);
-		UpdateBoundingBox();
-		
-	}
+        }
 	public void RotateClockwiseAboutYAxis(Point p, float degrees)
 	{
 		for(Triangle t : triangles)
 			t.RotateClockwiseAboutYAxis(p, degrees);
-		UpdateBoundingBox();
-		
 	}
 	public void RotateCounterClockwiseAboutXAxis(Point p, float degrees)
 	{
 		for(Triangle t : triangles)
 			t.RotateCounterClockwiseAboutXAxis(p, degrees);
-		UpdateBoundingBox();
-		
 	}
 	public void RotateClockwiseAboutXAxis(Point p, float degrees)
 	{
 		for(Triangle t : triangles)
 			t.RotateClockwiseAboutXAxis(p, degrees);
-		UpdateBoundingBox();
-		
 	}
 	public void RotateCounterClockwiseAboutZAxis(Point p, float degrees)
 	{
 		for(Triangle t : triangles)
 			t.RotateCounterClockwiseAboutZAxis(p, degrees);
-		UpdateBoundingBox();
-		
 	}
 	public void RotateClockwiseAboutZAxis(Point p, float degrees)
 	{
 		for(Triangle t : triangles)
 			t.RotateClockwiseAboutZAxis(p, degrees);
-		UpdateBoundingBox();
-		
 	}
 	public void Resize(float scaleFactor)
 	{
 		for(Triangle t : triangles)
 			t.Resize(scaleFactor);
-		UpdateBoundingBox();
-		
 	}
 	public Point GetCenter()
 	{
@@ -155,99 +210,12 @@ class RectangularPrism
 		totz /= 36.0d;
 		return new Point(totx, toty, totz);
 	}
-	public void DrawOutline(Graphics2D g)
+	public void Render(BufferedImage buf)
 	{
-		if(triangles[0].Should_Be_Drawn() && (!triangles[4].Should_Be_Drawn()) || !triangles[0].Should_Be_Drawn() && (triangles[4].Should_Be_Drawn()))
-		{
-			int[] coords = new int[4];
-			coords = Util.GetSharedSide(triangles[0], triangles[4]);
-			g.drawLine(coords[0] + xoffset, coords[1] + yoffset, coords[2] + xoffset, coords[3] + yoffset);
-		}
-		if(triangles[1].Should_Be_Drawn() && (!triangles[11].Should_Be_Drawn()) || !triangles[1].Should_Be_Drawn() && (triangles[11].Should_Be_Drawn()))
-		{
-			int[] coords = new int[4];
-			coords = Util.GetSharedSide(triangles[1], triangles[11]);
-			g.drawLine(coords[0] + xoffset, coords[1] + yoffset, coords[2] + xoffset, coords[3] + yoffset);
-		}
-		if(triangles[1].Should_Be_Drawn() && (!triangles[6].Should_Be_Drawn()) || !triangles[1].Should_Be_Drawn() && (triangles[6].Should_Be_Drawn()))
-		{
-			int[] coords = new int[4];
-			coords = Util.GetSharedSide(triangles[1], triangles[6]);
-			g.drawLine(coords[0] + xoffset, coords[1] + yoffset, coords[2] + xoffset, coords[3] + yoffset);
-		}
-		if(triangles[0].Should_Be_Drawn() && (!triangles[8].Should_Be_Drawn()) || !triangles[0].Should_Be_Drawn() && (triangles[8].Should_Be_Drawn()))
-		{
-			int[] coords = new int[4];
-			coords = Util.GetSharedSide(triangles[0], triangles[8]);
-			g.drawLine(coords[0] + xoffset, coords[1] + yoffset, coords[2] + xoffset, coords[3] + yoffset);
-		}
-		if(triangles[2].Should_Be_Drawn() && (!triangles[5].Should_Be_Drawn()) || !triangles[2].Should_Be_Drawn() && (triangles[5].Should_Be_Drawn()))
-		{
-			int[] coords = new int[4];
-			coords = Util.GetSharedSide(triangles[2], triangles[5]);
-			g.drawLine(coords[0] + xoffset, coords[1] + yoffset, coords[2] + xoffset, coords[3] + yoffset);
-		}
-		if(triangles[3].Should_Be_Drawn() && (!triangles[10].Should_Be_Drawn()) || !triangles[3].Should_Be_Drawn() && (triangles[10].Should_Be_Drawn()))
-		{
-			int[] coords = new int[4];
-			coords = Util.GetSharedSide(triangles[3], triangles[10]);
-			g.drawLine(coords[0] + xoffset, coords[1] + yoffset, coords[2] + xoffset, coords[3] + yoffset);
-		}
-		if(triangles[3].Should_Be_Drawn() && (!triangles[7].Should_Be_Drawn()) || !triangles[3].Should_Be_Drawn() && (triangles[7].Should_Be_Drawn()))
-		{
-			int[] coords = new int[4];
-			coords = Util.GetSharedSide(triangles[3], triangles[7]);
-			g.drawLine(coords[0] + xoffset, coords[1] + yoffset, coords[2] + xoffset, coords[3] + yoffset);
-		}
-		if(triangles[2].Should_Be_Drawn() && (!triangles[9].Should_Be_Drawn()) || !triangles[2].Should_Be_Drawn() && (triangles[9].Should_Be_Drawn()))
-		{
-			int[] coords = new int[4];
-			coords = Util.GetSharedSide(triangles[2], triangles[9]);
-			g.drawLine(coords[0] + xoffset, coords[1] + yoffset, coords[2] + xoffset, coords[3] + yoffset);
-		}
-		if(triangles[5].Should_Be_Drawn() && (!triangles[10].Should_Be_Drawn()) || !triangles[5].Should_Be_Drawn() && (triangles[10].Should_Be_Drawn()))
-		{
-			int[] coords = new int[4];
-			coords = Util.GetSharedSide(triangles[5], triangles[10]);
-			g.drawLine(coords[0] + xoffset, coords[1] + yoffset, coords[2] + xoffset, coords[3] + yoffset);
-		}
-		if(triangles[4].Should_Be_Drawn() && (!triangles[8].Should_Be_Drawn()) || !triangles[4].Should_Be_Drawn() && (triangles[8].Should_Be_Drawn()))
-		{
-			int[] coords = new int[4];
-			coords = Util.GetSharedSide(triangles[4], triangles[8]);
-			g.drawLine(coords[0] + xoffset, coords[1] + yoffset, coords[2] + xoffset, coords[3] + yoffset);
-		}
-		if(triangles[6].Should_Be_Drawn() && (!triangles[11].Should_Be_Drawn()) || !triangles[6].Should_Be_Drawn() && (triangles[11].Should_Be_Drawn()))
-		{
-			int[] coords = new int[4];
-			coords = Util.GetSharedSide(triangles[6], triangles[11]);
-			g.drawLine(coords[0] + xoffset, coords[1] + yoffset, coords[2] + xoffset, coords[3] + yoffset);
-		}
-		if(triangles[7].Should_Be_Drawn() && (!triangles[9].Should_Be_Drawn()) || !triangles[7].Should_Be_Drawn() && (triangles[9].Should_Be_Drawn()))
-		{
-			int[] coords = new int[4];
-			coords = Util.GetSharedSide(triangles[7], triangles[9]);
-			g.drawLine(coords[0] + xoffset, coords[1] + yoffset, coords[2] + xoffset, coords[3] + yoffset);
-		}
-	}
-	public void Render()
-	{
-		Color color = Color.blue;
-		boolean blue = true;
-		for(Triangle t : triangles)
-		{
-			if(t.Should_Be_Drawn())
-			{
-				Triangle triangle = World.camera.LookAt(t);
-				if(triangle != null)
-				{
-					blue = !blue;
-					color = blue ? Color.blue : Color.green;
-					triangle.SetColor(color);
-					World.triangles.add(triangle);
-				}
-			}
-		}
+            for(Triangle t : triangles)
+            {
+                t.Render(buf);
+            }
 	}
 	public void Move(Point target, float distance)
 	{
@@ -266,7 +234,13 @@ class RectangularPrism
 			t.points[2].SetY(t.points[2].GetExY() + (center.GetExY() - orig.GetExY()));
 			t.points[2].SetZ(t.points[2].GetExZ() + (center.GetExZ() - orig.GetExZ()));
 		}
-		UpdateBoundingBox();
-		
 	}
+	public void SetTexture(String file)
+	{
+            try
+            {
+                img = ImageIO.read(new File(file));
+            } 
+            catch (IOException ex) {}
+        }
 }
