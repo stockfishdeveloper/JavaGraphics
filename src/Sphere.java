3fe2897238c25c3ae632e56e8b028276ce4e471c
stockfishdeveloper <davidandjava@mail.com>
2016-11-30 11:29:21 -0600

    Clean up Sphere class
    It was a mess!

--- src/Sphere.java
+++ src/Sphere.java
@@ -1,47 +1,35 @@
-import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Polygon;
-import java.awt.Rectangle;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
-import javax.swing.JComponent;
-import javax.swing.SwingUtilities;
-@SuppressWarnings("serial")
-class Sphere extends JComponent implements MouseMotionListener, MouseListener, MouseWheelListener
+class Sphere
 {
-	int currx = 350;
-	int curry = 350;
-	static Point origin = new Point(350, 350, 0);
-	public int xoffset = 0;
-	public int yoffset = 0;
-	boolean isSelected = false;
-	private Rectangle BoundingBox = new Rectangle();
+	public int xoffset = 640;
+	public int yoffset = 500;
 	Point[] points;
 	Triangle[] triangles;
-	int layers = 0;
-	int ppl = 0;
+	BufferedImage img;
+	Pixel[][][] pixels;
+        Color color;
 	public Sphere()
 	{
 		
 	}
-	public Sphere(int layers, int ppl)
+        public Sphere(int layers, int ppl, Color c)
 	{
-		this.layers = layers * 2;
-		this.ppl = ppl;
+                color = c;
+                pixels = null;
+		int Layers = layers * 2;
+		int PPL = ppl;
 		triangles = new Triangle[(2 * layers * ( 2 * layers + ppl)) + (2 * ppl)];
-		for(int i = 0; i < triangles.length; i++) triangles[i] = new Triangle(new Point(0, 0, 0), new Point(0, 0, 0), new Point(0, 0, 0));
+		for(int i = 0; i < triangles.length; i++) triangles[i] = new Triangle(new Point(0, 0, 0), new Point(0, 0, 0), new Point(0, 0, 0), c);
 		int count = 0;
 		double[] semicircle = new double[layers * 2];
 		for(int i = -layers; i < layers; i++)
 		{
-			semicircle[count++] = Math.sqrt((double)((layers * layers) - (i * i)));
+                    semicircle[count++] = Math.sqrt((double)((layers * layers) - (i * i)));
 		}
 		count = 0;
 		int scount = 0;
@@ -50,147 +38,104 @@ class Sphere extends JComponent implements MouseMotionListener, MouseListener, M
 		points[count++] = new Point(0, layers / 2, 0);
 		for(int i = layers; i > -layers; i--)
 		{
-			for(int j = 0; j < ppl; j++)
-			{
-				points[count].SetY(i);
-				float degrees = (float)(j) * (360.0f / (float)(ppl));
-				Point p1 = new Point(semicircle[scount], 0, 0);
-				p1.RotateCounterClockwiseAboutYAxis(new Point(0, 0, 0), degrees);
-				points[count].SetX(p1.GetExX());
-				points[count++].SetZ(p1.GetExZ());
-			}
-			scount++;
+                    for(int j = 0; j < ppl; j++)
+                    {
+			points[count].SetY(i);
+			float degrees = (float)(j) * (360.0f / (float)(ppl));
+			Point p1 = new Point(semicircle[scount], 0, 0);
+			p1.RotateCounterClockwiseAboutYAxis(new Point(0, 0, 0), degrees);
+			points[count].SetX(p1.GetExX());
+			points[count++].SetZ(p1.GetExZ());
+                    }
+                    scount++;
 		}
 		points[count++] = new Point(0, -(layers / 2), 0);
 		//count = 0;
 		int count2 = 0;
 		for(int i = 0; i < ((2 * layers) - 2); i++)
 		{
-			for(int j = 0; j < ppl; j++)
+                    for(int j = 0; j < ppl; j++)
+                    {
+			int[] x = new int[3];
+			int[] y = new int[3];
+			int[] z = new int[3];
+			x[0] = points[(ppl * i) + j].GetX();
+			x[1] = points[(ppl * i) + j + 1].GetX();
+			x[2] = points[(ppl * i) + j + ppl + 1].GetX();
+			y[0] = points[(ppl * i) + j].GetY();
+			y[1] = points[(ppl * i) + j + 1].GetY();
+			y[2] = points[(ppl * i) + j + ppl + 1].GetY();
+			z[0] = points[(ppl * i) + j].GetZ();
+			z[1] = points[(ppl * i) + j + 1].GetZ();
+			z[2] = points[(ppl * i) + j + ppl + 1].GetZ();
+			triangles[count2++] = new Triangle(new Point(x[0], y[0], z[0]), new Point(x[1], y[1], z[1]), new Point(x[2], y[2], z[2]), c);
+			if(i > 0)
 			{
-					int[] x = new int[3];
-					int[] y = new int[3];
-					int[] z = new int[3];
-					x[0] = points[(ppl * i) + j].GetX() + xoffset;
-					x[1] = points[(ppl * i) + j + 1].GetX() + xoffset;
-					x[2] = points[(ppl * i) + j + ppl + 1].GetX() + xoffset;
-					y[0] = points[(ppl * i) + j].GetY() + yoffset;
-					y[1] = points[(ppl * i) + j + 1].GetY() + yoffset;
-					y[2] = points[(ppl * i) + j + ppl + 1].GetY() + yoffset;
-					z[0] = points[(ppl * i) + j].GetZ();
-					z[1] = points[(ppl * i) + j + 1].GetZ();
-					z[2] = points[(ppl * i) + j + ppl + 1].GetZ();
-					triangles[count2++] = new Triangle(new Point(x[0], y[0], z[0]), new Point(x[1], y[1], z[1]), new Point(x[2], y[2], z[2]));
-					if(i > 0)
-						{
-							x[0] = points[(ppl * i) + j].GetX() + xoffset;
-							x[1] = points[((ppl * i) + j) - ppl].GetX() + xoffset;
-							x[2] = points[(ppl * i) + j + 1].GetX() + xoffset;
-							y[0] = points[(ppl * i) + j].GetY() + yoffset;
-							y[1] = points[((ppl * i) + j) - ppl].GetY() + yoffset;
-							y[2] = points[(ppl * i) + j + 1].GetY() + yoffset;
-							z[0] = points[(ppl * i) + j].GetZ();
-							z[1] = points[((ppl * i) + j) - ppl].GetZ();
-							z[2] = points[(ppl * i) + j + 1].GetZ();
-							triangles[count2++] = new Triangle(new Point(x[0], y[0], z[0]), new Point(x[1], y[1], z[1]), new Point(x[2], y[2], z[2]));
-						}
+                            x[0] = points[(ppl * i) + j].GetX();
+                            x[1] = points[((ppl * i) + j) - ppl].GetX();
+                            x[2] = points[(ppl * i) + j + 1].GetX();
+                            y[0] = points[(ppl * i) + j].GetY();
+                            y[1] = points[((ppl * i) + j) - ppl].GetY();
+                            y[2] = points[(ppl * i) + j + 1].GetY();
+                            z[0] = points[(ppl * i) + j].GetZ();
+                            z[1] = points[((ppl * i) + j) - ppl].GetZ();
+                            z[2] = points[(ppl * i) + j + 1].GetZ();
+                            triangles[count2++] = new Triangle(new Point(x[0], y[0], z[0]), new Point(x[1], y[1], z[1]), new Point(x[2], y[2], z[2]), c);
 			}
+                    }
 		}
 		count2 = 0;
 		for(int j = 1; j < ppl; j++)
 		{
-				int[] x = new int[3];
-				int[] y = new int[3];
-				int[] z = new int[3];
-				x[0] = points[0].GetX() + xoffset;
-				x[1] = points[j].GetX() + xoffset;
-				x[2] = points[j + 1].GetX() + xoffset;
-				y[0] = points[0].GetY() + yoffset;
-				y[1] = points[j].GetY() + yoffset;
-				y[2] = points[j + 1].GetY() + yoffset;
-				z[0] = points[0].GetZ();
-				z[1] = points[j].GetZ();
-				z[2] = points[j + 1].GetZ();
-				triangles[count2++] = new Triangle(new Point(x[0], y[0], z[0]), new Point(x[1], y[1], z[1]), new Point(x[2], y[2], z[2]));
-		}
-		UpdateBoundingBox();
-		repaint();
-	}
-	public void UpdateBoundingBox()
-	{
-		int gx = -10000;
-		int ly = 10000;
-		int lx = 10000;
-		int gy = -10000;
-		for(Triangle t : triangles)
-		{
-			if(t.GreatestX() > gx) gx = t.GreatestX();
-			if(t.LeastX() < lx) lx = t.LeastX();
-			if(t.GreatestY() > gy) gy = t.GreatestY();
-			if(t.LeastY() < ly) ly = t.LeastY();
-		}
-		BoundingBox = new Rectangle();
-		BoundingBox.add(gx + xoffset, gy + yoffset);
-		BoundingBox.add(gx + xoffset, ly + yoffset);
-		BoundingBox.add(lx + xoffset, ly + yoffset);
-		BoundingBox.add(lx + xoffset, gy + yoffset);
-		this.setBounds(BoundingBox);
-	}
-	public void TranslateVisiblePosition(int x, int y)
-	{
-		xoffset += x;
-		yoffset += y;
-		UpdateBoundingBox();
-		repaint();
-	}
+                    int[] x = new int[3];
+                    int[] y = new int[3];
+                    int[] z = new int[3];
+                    x[0] = points[0].GetX();
+                    x[1] = points[j].GetX();
+                    x[2] = points[j + 1].GetX();
+                    y[0] = points[0].GetY();
+                    y[1] = points[j].GetY();
+                    y[2] = points[j + 1].GetY();
+                    z[0] = points[0].GetZ();
+                    z[1] = points[j].GetZ();
+                    z[2] = points[j + 1].GetZ();
+                    triangles[count2++] = new Triangle(new Point(x[0], y[0], z[0]), new Point(x[1], y[1], z[1]), new Point(x[2], y[2], z[2]), c);
+                }
+        }
 	public void RotateCounterClockwiseAboutYAxis(Point p, float degrees)
 	{
 		for(Triangle t : triangles)
-		t.RotateCounterClockwiseAboutYAxis(p, degrees);
-		UpdateBoundingBox();
-		repaint();
-	}
+			t.RotateCounterClockwiseAboutYAxis(p, degrees);
+        }
 	public void RotateClockwiseAboutYAxis(Point p, float degrees)
 	{
 		for(Triangle t : triangles)
-		t.RotateClockwiseAboutYAxis(p, degrees);
-		UpdateBoundingBox();
-		repaint();
+			t.RotateClockwiseAboutYAxis(p, degrees);
 	}
 	public void RotateCounterClockwiseAboutXAxis(Point p, float degrees)
 	{
 		for(Triangle t : triangles)
-		t.RotateCounterClockwiseAboutXAxis(p, degrees);
-		UpdateBoundingBox();
-		repaint();
+			t.RotateCounterClockwiseAboutXAxis(p, degrees);
 	}
 	public void RotateClockwiseAboutXAxis(Point p, float degrees)
 	{
 		for(Triangle t : triangles)
-		t.RotateClockwiseAboutXAxis(p, degrees);
-		UpdateBoundingBox();
-		repaint();
+			t.RotateClockwiseAboutXAxis(p, degrees);
 	}
 	public void RotateCounterClockwiseAboutZAxis(Point p, float degrees)
 	{
 		for(Triangle t : triangles)
-		t.RotateCounterClockwiseAboutZAxis(p, degrees);
-		UpdateBoundingBox();
-		repaint();
+			t.RotateCounterClockwiseAboutZAxis(p, degrees);
 	}
 	public void RotateClockwiseAboutZAxis(Point p, float degrees)
 	{
 		for(Triangle t : triangles)
-		t.RotateClockwiseAboutZAxis(p, degrees);
-		UpdateBoundingBox();
-		repaint();
+			t.RotateClockwiseAboutZAxis(p, degrees);
 	}
 	public void Resize(float scaleFactor)
 	{
 		for(Triangle t : triangles)
 			t.Resize(scaleFactor);
-		UpdateBoundingBox();
-		repaint();
 	}
 	public Point GetCenter()
 	{
@@ -209,50 +154,25 @@ class Sphere extends JComponent implements MouseMotionListener, MouseListener, M
 			totz += t.points[1].GetExZ();
 			totz += t.points[2].GetExZ();
 		}
-		totx /= (triangles.length * 3);
-		toty /= (triangles.length * 3);
-		totz /= (triangles.length * 3);
+		totx /= 3 * triangles.length;
+		toty /= 3 * triangles.length;
+		totz /= 3 * triangles.length;
 		return new Point(totx, toty, totz);
 	}
-	public void DrawOutline(Graphics2D g)
+	public void Render(BufferedImage buf)
 	{
-		for(int i = 0; i < triangles.length - ppl; i++)
-		{
-			if((triangles[i].Should_Be_Drawn() && !triangles[i + (ppl - 1)].Should_Be_Drawn()) || (!triangles[i].Should_Be_Drawn() && triangles[i + (ppl - 1)].Should_Be_Drawn()))
-			{
-				int[] coords = new int[4];
-				coords = Util.GetSharedSide(triangles[i], triangles[i + (ppl - 1)]);
-				g.drawLine(coords[0] + xoffset, coords[1] + yoffset, coords[2] + xoffset, coords[3] + yoffset);
-			}
-		}
+            for(Triangle t : triangles)
+            {
+                t.Render(buf);
+            }
 	}
-	public void paintComponent(Graphics gr)
+        /*public void Render(Graphics2D g)
 	{
-		Graphics2D g = (Graphics2D)gr;
-		boolean blue = true;
-		for(Triangle t : triangles)
-		{
-			if(t.Should_Be_Drawn())
-			{
-				g.setColor(blue ? Color.blue : Color.red);
-				int[] x = new int[3];
-				int[] y = new int[3];
-				for(int i = 0; i < 3; i++)
-				{
-					x[i] = t.points[i].GetX() + xoffset;
-					y[i] = t.points[i].GetY() + yoffset;
-				}
-				Polygon poly = new Polygon(x, y, 3);
-				g.fillPolygon(poly);
-			}
-		}
-		if(isSelected)
-		{
-			g.setStroke(new BasicStroke(3));
-			g.setColor(Color.green);
-			DrawOutline(g);
-		}
-	}
+            for(Triangle t : triangles)
+            {
+                t.Render(g);
+            }
+	}*/
 	public void Move(Point target, float distance)
 	{
 		Point center = GetCenter();
@@ -270,85 +190,13 @@ class Sphere extends JComponent implements MouseMotionListener, MouseListener, M
 			t.points[2].SetY(t.points[2].GetExY() + (center.GetExY() - orig.GetExY()));
 			t.points[2].SetZ(t.points[2].GetExZ() + (center.GetExZ() - orig.GetExZ()));
 		}
-		UpdateBoundingBox();
-		repaint();
 	}
-	public void mouseDragged(MouseEvent m) {
-		Component c = (Component) World.panel.getComponentAt(m.getX(), m.getY());
-		if(c != null && c == (this) && isSelected == true)
-		{
-			if(SwingUtilities.isRightMouseButton(m))
-			{
-				if(m.getX() < currx)
-				{	
-					currx = m.getX();
-					RotateCounterClockwiseAboutYAxis(GetCenter(), 3.0f);
-				}
-				if(m.getX() > currx)
-				{
-					currx = m.getX();
-					RotateClockwiseAboutYAxis(GetCenter(), 3.0f);
-				}
-				if(m.getY() > curry)
-				{
-					curry = m.getY();
-					RotateCounterClockwiseAboutXAxis(GetCenter(), 3.0f);
-				}
-				if(m.getY() < curry)
-				{
-					curry = m.getY();
-					RotateClockwiseAboutXAxis(GetCenter(), 3.0f);
-				}
-			}
-		}
-		else if(SwingUtilities.isLeftMouseButton(m))
-		{
-			TranslateVisiblePosition(m.getX() - currx, m.getY() - curry);
-			currx = m.getX();
-			curry = m.getY();
-		}
-	}
-	public void mouseMoved(MouseEvent m) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void mouseClicked(MouseEvent m) {
-		Component c = (Component) World.panel.getComponentAt(m.getX(), m.getY());
-		if(c != null && c == (this) && SwingUtilities.isLeftMouseButton(m))
-		{
-		isSelected = (isSelected == true ? false : true);
-		repaint();
-		}
-	}
-	@Override
-	public void mouseEntered(MouseEvent m) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseExited(MouseEvent m) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mousePressed(MouseEvent m) {
-		Component c = (Component) World.panel.getComponentAt(m.getX(), m.getY());
-		if(c != null && c == (this) && SwingUtilities.isLeftMouseButton(m))
-		{
-			currx = m.getX();
-			curry = m.getY();
-		}
-	}
-	@Override
-	public void mouseReleased(MouseEvent m) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseWheelMoved(MouseWheelEvent m) {
-		if(m.getPreciseWheelRotation() < 0)
-			Resize(1.1f);
-		else
-			Resize(0.9f);		
-	}
+	public void SetTexture(String file)
+	{
+            try
+            {
+                img = ImageIO.read(new File(file));
+            } 
+            catch (IOException ex) {}
+        }
 }
