f64a141d5fb82a402510e84ec3eb7d56e3bea9c2
stockfishdeveloper <davidandjava@mail.com>
2016-12-07 10:47:11 -0600

    Allow textured objects to animate right
    When two textured objects are touching, they can now "mesh" and form one smooth animation

--- src/Triangle.java
+++ src/Triangle.java
@@ -133,30 +133,38 @@ class Triangle implements Comparable<Triangle>
                         {
                             Point p = World.camera.LookAt(pixels[i]);
                             if(p != null)
-                            if(p.GetX() > -640 && p.GetX() < 640 && p.GetY() > -500 && p.GetY() < 500)
-                            image.setRGB(p.GetX() + 640, p.GetY() + 500, pixels[i].GetColor());
+                            {
+                            	if(p.GetX() > -640 && p.GetX() < 640 && p.GetY() > -500 && p.GetY() < 500)
+                            	{
+                            		if(World.distancefromscreen[(int) Math.round(p.GetExX()) + 640][(int) Math.round(p.GetExY()) + 500] > p.GetExZ())
+                            		{
+                            			image.setRGB((int) Math.round(p.GetExX()) + 640, (int) Math.round(p.GetExY()) + 500, pixels[i].GetColor());
+                            			World.distancefromscreen[(int) Math.round(p.GetExX()) + 640][(int) Math.round(p.GetExY()) + 500] = p.GetExZ();
+                            		}
+                            	}
+                            }
                         }
                     }
-                else
-                {
-                    Graphics2D g = image.createGraphics();
-                    g.setColor(color);
-                    int[] x = new int[3];
-                    int[] y = new int[3];
-                    Triangle t = World.camera.LookAt(this);
-                    for(int i = 0; i < 3; i++)
-                    {                        
-                        if(t != null)
-                        {
-                            x[i] = t.points[i].GetX() + 640;
-                            y[i] = t.points[i].GetY() + 500;
-                        }
-                    }
-                    if(t != null)
+                    else
                     {
-                        Polygon poly = new Polygon(x, y, 3);
-                        g.fillPolygon(poly);
-                    }
+                    	Graphics2D g = image.createGraphics();
+                    	g.setColor(color);
+                    	int[] x = new int[3];
+                    	int[] y = new int[3];
+                    	Triangle t = World.camera.LookAt(this);
+                    	for(int i = 0; i < 3; i++)
+                    	{                        
+                    		if(t != null)
+                    		{
+                    			x[i] = t.points[i].GetX()  + 640;
+                    			y[i] = t.points[i].GetY() + 500;
+                    		}
+                    	}
+                    	if(t != null)
+                    	{
+                    		Polygon poly = new Polygon(x, y, 3);
+                    		g.fillPolygon(poly);
+                    	}
                 }
             }
 	}
@@ -174,12 +182,17 @@ class Triangle implements Comparable<Triangle>
             Polygon poly = new Polygon(x, y, 3);
             g.fillPolygon(poly);
         }*/
-	public void Draw_Mesh(Graphics2D g)
+	public void Draw_Mesh(BufferedImage buf)
 	{
-                Triangle t = new Triangle(World.camera.LookAt(this));
-		g.drawLine(t.points[0].GetX() + 640, t.points[0].GetY() + 500, t.points[1].GetX() + 640, t.points[1].GetY() + 500);
-		g.drawLine(t.points[1].GetX() + 640, t.points[1].GetY() + 500, t.points[2].GetX() + 640, t.points[2].GetY() + 500);
-		g.drawLine(t.points[0].GetX() + 640, t.points[0].GetY() + 500, t.points[2].GetX() + 640, t.points[2].GetY() + 500);
+            if(Should_Be_Drawn())
+		{
+            Graphics2D g = buf.createGraphics();
+            g.setColor(color);
+            Triangle t = new Triangle(World.camera.LookAt(this));
+            g.drawLine(t.points[0].GetX() + 640, t.points[0].GetY() + 500, t.points[1].GetX() + 640, t.points[1].GetY() + 500);
+            g.drawLine(t.points[1].GetX() + 640, t.points[1].GetY() + 500, t.points[2].GetX() + 640, t.points[2].GetY() + 500);
+            g.drawLine(t.points[0].GetX() + 640, t.points[0].GetY() + 500, t.points[2].GetX() + 640, t.points[2].GetY() + 500);
+                }
 	}
 	public int GreatestX()
 	{
