b17cf0a1a181af01d2dd67463e85daa1b09c2a76
stockfishdeveloper <davidandjava@mail.com>
2017-01-05 13:52:30 -0600

    Remove UpdateBounds function
    This is useless for now because I can calculate if a Point is in a Frustum a different way

--- src/Frustum.java
+++ src/Frustum.java
@@ -19,7 +19,6 @@ class Frustum
 		leasty = 10000000;
 		greatestz = -10000000;
 		leastz = 10000000;
-		UpdateBounds();
 	}
 	public Frustum(Frustum frustum)
 	{
@@ -32,61 +31,31 @@ class Frustum
 	{
 		for(Point p1 : points)
 			p1.RotateCounterClockwiseAboutYAxis(p, degrees);
-		UpdateBounds();
 	}
 	public void RotateClockwiseAboutYAxis(Point p, float degrees)
 	{
 		for(Point p1 : points)
 			p1.RotateClockwiseAboutYAxis(p, degrees);
-		UpdateBounds();
 	}
 	public void RotateCounterClockwiseAboutXAxis(Point p, float degrees)
 	{
 		for(Point p1 : points)
 			p1.RotateCounterClockwiseAboutXAxis(p, degrees);
-		UpdateBounds();
 	}
 	public void RotateClockwiseAboutXAxis(Point p, float degrees)
 	{
 		for(Point p1 : points)
 			p1.RotateClockwiseAboutXAxis(p, degrees);
-		UpdateBounds();
 	}
 	public void RotateCounterClockwiseAboutZAxis(Point p, float degrees)
 	{
 		for(Point p1 : points)
 			p1.RotateCounterClockwiseAboutZAxis(p, degrees);
-		UpdateBounds();
 	}
 	public void RotateClockwiseAboutZAxis(Point p, float degrees)
 	{
 		for(Point p1 : points)
 			p1.RotateClockwiseAboutZAxis(p, degrees);
-		UpdateBounds();
-	}
-	public void UpdateBounds()
-	{
-        greatestx = -10000000;
-		leastx = 10000000;
-		greatesty = -10000000;
-		leasty = 10000000;
-		greatestz = -10000000;
-		leastz = 10000000;
-		for(int i = 0; i < 8; i++)
-		{
-			if(points[i].GetExX() < leastx)
-				leastx = points[i].GetExX();
-			if(points[i].GetExX() > greatestx)
-				greatestx = points[i].GetExX();
-			if(points[i].GetExY() < leasty)
-				leasty = points[i].GetExY();
-			if(points[i].GetExY() > greatesty)
-				greatesty = points[i].GetExY();
-			if(points[i].GetExZ() < leastz)
-				leastz = points[i].GetExZ();
-			if(points[i].GetExZ() > greatestz)
-				greatestz = points[i].GetExZ();
-		}
 	}
 	public boolean Contains(Point p)
         {
