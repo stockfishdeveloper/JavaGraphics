45e54545a7baaf1e0bf28f6a248a30ecf93cc75c
stockfishdeveloper <davidandjava@mail.com>
2017-01-05 13:54:08 -0600

    Change test code

--- src/Run.java
+++ src/Run.java
@@ -3,24 +3,35 @@ import javax.swing.JPanel;
 
 class Run
 {
-            public static JFrame frame;
-            public static JPanel panel;
-            public static void main(String[] args) throws InterruptedException
+    public static JFrame frame;
+    public static JPanel panel;
+    public static Long nextframe;
+    public static void main(String[] args) throws InterruptedException
+    {
+    	World w = new World();
+        frame = new JFrame();
+        frame.setSize(1280, 1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new JPanel();
+        panel.setLayout(null);
+        panel.add(w);
+        panel.addMouseMotionListener(w);
+        panel.addMouseWheelListener(w);
+        panel.setFocusable(true);
+        panel.addKeyListener(w);
+        frame.add(panel);
+        frame.setVisible(true);
+        nextframe = System.currentTimeMillis();
+        while(true)
+        {
+        	if(System.currentTimeMillis() >= nextframe)
             {
-            World w = new World();
-            frame = new JFrame();
-            frame.setSize(1280, 1000);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            panel = new JPanel();
-            panel.setLayout(null);
-            panel.add(w);
-            panel.addMouseMotionListener(w);
-            panel.addMouseWheelListener(w);
-            frame.add(panel);
-            frame.setVisible(true);
-            while(true){
-            	w.repaint();
-            	Thread.sleep(10);
-              }
+        		//w.camera.RotateClockwiseAboutYAxis(new Point(0, 0, 0), 1f);
+        		//World.camera.Print_Info();
+        		//System.out.println(Util.Distance_Between(World.camera.GetLeft(), new Point(0, 0, 0)));
+        		w.repaint();
+        		nextframe += 100;
             }
+        }
+    }
 }
