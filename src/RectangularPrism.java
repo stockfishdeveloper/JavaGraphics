350552bce394bdf8dd5f6b7184d511aaf75dcf37
stockfishdeveloper <davidandjava@mail.com>
2017-01-31 11:36:44 -0600

    Update RectangularPrism rendering method

--- src/RectangularPrism.java
+++ src/RectangularPrism.java
@@ -2,9 +2,8 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-
 import javax.imageio.ImageIO;
+
 class RectangularPrism
 {
 	public int xoffset = 640;
@@ -13,12 +12,15 @@ class RectangularPrism
 	Triangle[] triangles = new Triangle[12];
 	BufferedImage img;
 	Pixel[][][] pixels;
+        Color color;
 	public RectangularPrism()
 	{
 		
 	}
-	public RectangularPrism(Point center, int length, int width, int height, String texture)
+        public RectangularPrism(Point center, int length, int width, int height, Color c)
 	{
+                color = c;
+                pixels = null;
 		for(int i = 0; i < 8; i++) points[i] = new Point(0, 0, 0);
 		points[0].SetX(center.GetExX() - (length / 2));
 		points[0].SetY(center.GetExY() + (height / 2));
@@ -44,115 +46,19 @@ class RectangularPrism
 		points[7].SetX(center.GetExX() + (length / 2));
 		points[7].SetY(center.GetExY() - (height / 2));
                 points[7].SetZ(center.GetExZ() + (width / 2));
-		try {
-            img = ImageIO.read(new File(texture));
-        } catch (IOException ex) {
-         		}
-		pixels = new Pixel[6][img.getWidth()][img.getHeight()];
-		double unitsperpixel = (double)((length * 2) / (double)(img.getWidth()));
-		//Front
-		for(int i = 0; i < img.getWidth(); i++)
-		{
-			for(int j = 0; j < img.getHeight(); j++)
-			{
-				pixels[0][i][j] = new Pixel((unitsperpixel * (i - (img.getWidth() / 2))), (unitsperpixel * (j - (img.getHeight() / 2))), center.GetExZ() - length, new Color(img.getRGB(i, j)));
-			}
-		}
-		//Back
-		for(int i = 0; i < img.getWidth(); i++)
-		{
-			for(int j = 0; j < img.getHeight(); j++)
-			{
-				pixels[1][i][j] = new Pixel((unitsperpixel * (i - (img.getWidth() / 2))), (unitsperpixel * (j - (img.getHeight() / 2))), center.GetExZ() + length, new Color(img.getRGB(i, j)));
-			}
-		}
-		//Top
-		for(int i = 0; i < img.getWidth(); i++)
-		{
-			for(int j = 0; j < img.getHeight(); j++)
-			{
-				pixels[2][i][j] = new Pixel((unitsperpixel * (i - (img.getWidth() / 2))),  center.GetExY() + length, (unitsperpixel * (j - (img.getHeight() / 2))), new Color(img.getRGB(i, j)));
-			}
-		}
-		//Bottom
-		for(int i = 0; i < img.getWidth(); i++)
-		{
-			for(int j = 0; j < img.getHeight(); j++)
-			{
-				pixels[3][i][j] = new Pixel((unitsperpixel * (i - (img.getWidth() / 2))),  center.GetExY() - length, (unitsperpixel * (j - (img.getHeight() / 2))), new Color(img.getRGB(i, j)));
-			}
-		}
-		//Left
-		for(int i = 0; i < img.getWidth(); i++)
-		{
-			for(int j = 0; j < img.getHeight(); j++)
-			{
-				pixels[4][i][j] = new Pixel(center.GetExX() - length, (unitsperpixel *  (j - (img.getWidth() / 2))), (unitsperpixel * (i - (img.getHeight() / 2))), new Color(img.getRGB(i, j)));
-			}
-		}
-		//Right
-		for(int i = 0; i < img.getWidth(); i++)
-		{
-			for(int j = 0; j < img.getHeight(); j++)
-			{
-				pixels[5][i][j] = new Pixel(center.GetExX() + length, (unitsperpixel * (j - (img.getWidth() / 2))), (unitsperpixel * (i - (img.getHeight() / 2))), new Color(img.getRGB(i, j)));
-			}
-		}
-		ArrayList<Pixel> pix = new ArrayList<>();
-		ArrayList<Pixel> pix2 = new ArrayList<>();
-		int inc = img.getWidth();
-		for(int p = 0; p < 6; p++)
-		{
-			for(int i = 0; i < img.getWidth(); i++)
-			{
-				for(int j = 0; j < img.getHeight(); j++)
-				{
-					if(j < inc)
-					{
-						pix.add(pixels[p][i][j]);
-					}
-					else
-					{
-						pix2.add(pixels[p][i][j]);
-					}
-				}
-				inc--;
-			}
-			if(p == 0)
-			{
-				triangles[0] = new Triangle(points[0], points[1], points[2], pix);
-				triangles[1] = new Triangle(points[1], points[3], points[2], pix2);
-			}
-			else if(p == 1)
-			{
-				triangles[2] = new Triangle(points[5], points[4], points[7], pix);
-				triangles[3] = new Triangle(points[4], points[6], points[7], pix2);
-			}
-			else if(p == 2)
-			{
-				triangles[4] = new Triangle(points[4], points[5], points[0], pix);
-				triangles[5] = new Triangle(points[5], points[1], points[0], pix2);
-			}
-			else if(p == 3)
-			{
-				triangles[6] = new Triangle(points[2], points[3], points[6], pix);
-				triangles[7] = new Triangle(points[3], points[7], points[6], pix2);
-			}
-			else if(p == 4)
-			{
-				triangles[8] = new Triangle(points[4], points[0], points[6], pix);
-				triangles[9] = new Triangle(points[0], points[2], points[6], pix2);
-			}
-			else if(p == 5)
-			{
-				triangles[10] = new Triangle(points[1], points[5], points[3], pix);
-				triangles[11] = new Triangle(points[5], points[7], points[3], pix2);
-			}
-			inc = img.getWidth();
-			pix.clear();
-			pix2.clear();
-		}
-	}
+                triangles[0] = new Triangle(points[0], points[1], points[2], c);
+		triangles[1] = new Triangle(points[1], points[3], points[2], c);
+		triangles[2] = new Triangle(points[5], points[4], points[7], c);
+		triangles[3] = new Triangle(points[4], points[6], points[7], c);
+		triangles[4] = new Triangle(points[4], points[5], points[0], c);
+		triangles[5] = new Triangle(points[5], points[1], points[0], c);
+		triangles[6] = new Triangle(points[2], points[3], points[6], c);
+		triangles[7] = new Triangle(points[3], points[7], points[6], c);
+		triangles[8] = new Triangle(points[4], points[0], points[6], c);
+		triangles[9] = new Triangle(points[0], points[2], points[6], c);
+		triangles[10] = new Triangle(points[1], points[5], points[3], c);
+		triangles[11] = new Triangle(points[5], points[7], points[3], c);
+        }
 	public void RotateCounterClockwiseAboutYAxis(Point p, float degrees)
 	{
 		for(Triangle t : triangles)
@@ -217,13 +123,13 @@ class RectangularPrism
                 t.Render(buf);
             }
 	}
-        public void Draw_Mesh(BufferedImage buf)
-	{
-            for(Triangle t : triangles)
-            {
-                t.Draw_Mesh(buf);
-            }
+    public void Draw_Mesh(BufferedImage buf)
+    {
+        for(Triangle t : triangles)
+        {
+            t.Draw_Mesh(buf);
         }
+    }
 	public void Move(Point target, float distance)
 	{
 		Point center = GetCenter();
