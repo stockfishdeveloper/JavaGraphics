cb78be554af293dc5478e9d456e8569d855b304c
stockfishdeveloper <davidandjava@mail.com>
2017-02-13 08:45:11 -0600

    Remove unneeded variables

--- src/Frustum.java
+++ src/Frustum.java
@@ -1,24 +1,12 @@
 class Frustum
 {
 	Point[] points = new Point[8];
-	double greatestx;
-	double leastx;
-	double greatesty;
-	double leasty;
-	double greatestz;
-	double leastz;
 	public Frustum(Point[] points1)
 	{
 		for(int i = 0; i < 8; i++)
 		{
 			points[i] = points1[i];
 		}
-		greatestx = -10000000;
-		leastx = 10000000;
-		greatesty = -10000000;
-		leasty = 10000000;
-		greatestz = -10000000;
-		leastz = 10000000;
 	}
 	public Frustum(Frustum frustum)
 	{
@@ -56,11 +44,5 @@ class Frustum
 	{
 		for(Point p1 : points)
 			p1.RotateClockwiseAboutZAxis(p, degrees);
-	}
-	public boolean Contains(Point p)
-        {
-		return (//p.GetExX() > leastx && p.GetExX() < greatestx && 
-				//p.GetExY() > leasty && p.GetExY() < greatesty &&
-				p.GetExZ() > leastz /*&& p.GetExZ() < greatestz*/);
 	}
 }
