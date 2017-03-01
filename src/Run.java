d3d26ddeceb4e2ad35d4f72519bff3ef3944d8c7
stockfishdeveloper <davidandjava@mail.com>
2017-03-01 11:23:22 -0600

    Change test code

--- src/Run.java
+++ src/Run.java
@@ -8,8 +8,10 @@ class Run
     public static JFrame frame;
     public static JPanel panel;
     public static Long nextframe;
+    static Camera cam;
     public static void main(String[] args) throws InterruptedException, IOException
     {
+    	cam = new Camera();
     	World w = new World();
         frame = new JFrame();
         frame.setSize(1280, 1000);
@@ -24,17 +26,21 @@ class Run
         frame.add(panel);
         frame.setVisible(true);
         nextframe = System.currentTimeMillis();
-        //World.camera.MoveUp(400);
-        Mesh mesh = new Mesh("L:\\teapot.obj");
+        //Point p = new Point(10, 0, 0);
+        //p.RotateCounterClockwiseAboutYAxis(new Point(2, 0, 0), 180);
+        //Point p1 = w.camera.LookAt(p);
+        //p1.Print_Info();
         while(true)
         {
         	if(System.currentTimeMillis() >= nextframe)
             {
-        		//w.camera.RotateClockwiseAboutUpAxis(1f);
+        		//w.camera.RotMat.Output();
+        		//w.cube.RotateClockwiseAboutYAxis(new Point(0, 0, 0), 1.5f);
+                //w.cube.RotateClockwiseAboutXAxis(new Point(0, 0, 0), 1.5f);
         		//World.camera.MoveBackward(50);
-                //System.out.println(Util.Distance_Between(World.camera.GetLeft(), new Point(0, 0, 0)));
+                //System.out.println(Util.Distance_Between(World.camera.GetDirection(), new Point(0, 0, 0)));
         		w.repaint();
-                nextframe += 150;
+                nextframe += 100;
             }
         }
     }
