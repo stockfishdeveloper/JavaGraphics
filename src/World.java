3e8bb6ecc8f7ae26ea1897571b6800c1a626fb53
stockfishdeveloper <davidandjava@mail.com>
2017-03-21 09:49:18 -0500

    Change test code

--- src/World.java
+++ src/World.java
@@ -8,166 +8,238 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JComponent;
 
-class World extends JComponent implements MouseMotionListener, MouseListener, MouseWheelListener, KeyListener
-{
+class World extends JComponent implements Runnable, MouseMotionListener,
+		MouseListener, MouseWheelListener, KeyListener {
 	private static final long serialVersionUID = 1L;
+	private Thread thread;
+	private float fps;
+	private boolean running;
+	private int frames;
 	static BufferedImage screen;
 	BufferedImage blank;
 	int currx = 0;
 	int curry = 0;
-	public Cube cube;
 	public static Camera camera;
 	SampleMesh sample;
 	static double[][] distancefromscreen = new double[1280][1000];
-	ArrayList<MouseEvent> events;
-	       
-	public World()
-	{
-		cube = new Cube(new Point(0, 0, 0),10,"L:\\woodend1.png"/*Color.black*/);
+	volatile ArrayList<MouseEvent> events;
+	public ArrayList<Cube> cubes;
+
+	public World() {
+		frames = 0;
+		fps = 0;
+		thread = new Thread(this);
+		cubes = new ArrayList<>();
+		boolean blue = true;
+		for (int j = 0; j < 10; j++)
+			for (int v = 0; v < 10; v++) {
+				cubes.add(new Cube(new Point(j * 100, 0, v * 100), 10,
+				(blue ? Color.blue : Color.red)));
+				blue = !blue;
+			}
+		//cubes.add(new Cube(new Point(-5, 0, 40),10,"L:\\woodend1.png"/*Color.blue*/));
+		//cubes.add(new Cube(new Point(5, 0, 40),10,"L:\\woodend1.png"/*Color.red*/));
 		events = new ArrayList<>();
-        camera = new Camera();
-        camera.MoveBackward(20); 
-        //camera.MoveUp(100);
-        sample = new SampleMesh(new Point(0, 0, 0), Color.red, 100);
+		camera = new Camera();
+		camera.MoveBackward(20);
+		sample = new SampleMesh(new Point(0, 0, 0), Color.red, 50);
 		screen = new BufferedImage(1280, 1000, BufferedImage.TYPE_INT_RGB);
-        blank = new BufferedImage(1280, 1000, BufferedImage.TYPE_INT_RGB);
-        for(int i = 0; i < 1280; i++)
-        {
-        	for(int j = 0; j < 1000; j++)
-            {
-            	distancefromscreen[i][j] = Double.MAX_VALUE;
-            }
-        }
-        for(int i = 0; i < screen.getWidth(); i++)
-		{
-            for(int j = 0; j < screen.getHeight(); j++)
-			{
-            	screen.setRGB(i, j, 16777215);
-            	blank.setRGB(i, j, 16777215);
+		blank = new BufferedImage(1280, 1000, BufferedImage.TYPE_INT_RGB);
+		for (int i = 0; i < 1280; i++) {
+			for (int j = 0; j < 1000; j++) {
+				distancefromscreen[i][j] = Double.MAX_VALUE;
 			}
 		}
-        this.setBounds(0, 0, 1280, 1000);
-	}
-	void ApplyEvents()
-	{
-		for(MouseEvent m : events)
-		{
-			if(m.getX() > currx)
-	        {
-				camera.RotateClockwiseAboutUpAxis(2f);
-	        }
-			else if(m.getX() < currx)
-	        {
-				camera.RotateCounterClockwiseAboutUpAxis(2f);
-	        }
-			if(m.getY() > curry)
-	        {
-				camera.RotateClockwiseAboutRightAxis(2f);
-	        }
-			else if(m.getY() < curry)
-	        {
-				camera.RotateCounterClockwiseAboutRightAxis(2f);
-	        }
-			currx = m.getX();
-			curry = m.getY();
+		for (int i = 0; i < screen.getWidth(); i++) {
+			for (int j = 0; j < screen.getHeight(); j++) {
+				screen.setRGB(i, j, 16777215);
+				blank.setRGB(i, j, 16777215);
+			}
 		}
-		events.clear();
+		this.setBounds(0, 0, 1280, 1000);
+		start();
 	}
-	@Override
-    public void paintComponent(Graphics gr)
-	{
-		ApplyEvents();
-		Graphics2D g = (Graphics2D)gr;
-		Graphics2D graphics = screen.createGraphics();
-		graphics.fillRect(0, 0, 1280, 1000);
-        //sample.Draw_Mesh(screen);
-		cube.Render(screen);
-		g.drawImage(screen, 0, 0, 1280, 1000, null);
-        for(int i = 0; i < 1280; i++)
-        {
-        	for(int j = 0; j < 1000; j++)
-        	{
-        		distancefromscreen[i][j] = Double.MAX_VALUE;
-        	}
-        }
-        }
+
+	private synchronized void start() {
+		running = true;
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	synchronized void ApplyEvents() {
+		try {
+			for (MouseEvent m : events) {
+				if (m.getX() > currx) {
+					camera.RotateClockwiseAboutUpAxis(2f);
+				} else if (m.getX() < currx) {
+					camera.RotateCounterClockwiseAboutUpAxis(2f);
+				}
+				/*if (m.getY() > curry) {
+					camera.RotateClockwiseAboutRightAxis(2f);
+				} else if (m.getY() < curry) {
+					camera.RotateCounterClockwiseAboutRightAxis(2f);
+				}*/
+				currx = m.getX();
+				curry = m.getY();
+			}
+			events.clear();
+		} catch (java.util.ConcurrentModificationException c) {
+		}
+	}
+
+	public void render() {
+		// ApplyEvents();
+		frames++;
+		Graphics2D g1 = (Graphics2D) screen.getGraphics();
+		g1.setBackground(new Color(255, 255, 255, 0));
+		g1.clearRect(0, 0, 1280, 1000);
+		sample.Render(screen);
+		for(Cube c : cubes)
+			c.Render(screen);
+		BufferStrategy bs = Run.frame.getBufferStrategy();
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(screen, 0, 0, screen.getWidth(), screen.getHeight(), null);
+		g.drawString("FPS: " + fps, 50, 50);
+		g.drawString("MS per frame: " + 1000 / fps, 50, 65);
+		g.dispose();
+		bs.show();
+		for (int i = 0; i < 1280; i++) {
+			for (int j = 0; j < 1000; j++) {
+				distancefromscreen[i][j] = Double.MAX_VALUE;
+			}
+		}
+	}
+
 	@Override
 	public void mouseDragged(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mouseMoved(MouseEvent m) {
 		events.add(m);
 	}
+
 	@Override
 	public void mouseWheelMoved(MouseWheelEvent m) {
-		if(m.getPreciseWheelRotation() < 0)
+		if (m.getPreciseWheelRotation() < 0)
 			camera.MoveForward(10);
 		else
-			camera.MoveBackward(10);	
-        }
+			camera.MoveBackward(10);
+	}
+
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mouseExited(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mousePressed(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-    @Override
-    public void keyTyped(KeyEvent k) {
-        //System.out.println("Key typed!");
-    }
+	@Override
+	public void keyTyped(KeyEvent k) {
+		// System.out.println("Key typed!");
+	}
 
-    @Override
-    public void keyPressed(KeyEvent k) {
-        if(k.getKeyCode() == KeyEvent.VK_LEFT)
-            camera.MoveLeft(3.0f);
-        else if(k.getKeyCode() == KeyEvent.VK_RIGHT)
-            camera.MoveRight(3.0f);
-        else if(k.getKeyCode() == KeyEvent.VK_UP)
-            camera.MoveUp(3.0f);
-        else if(k.getKeyCode() == KeyEvent.VK_DOWN)
-            camera.MoveDown(3.0f);
-        else if(k.getKeyCode() == KeyEvent.VK_CONTROL)
-            camera.Print_Info();
-        else if(k.getKeyCode() == KeyEvent.VK_A)
-            camera.RotateCounterClockwiseAboutUpAxis(1f);
-        else if(k.getKeyCode() == KeyEvent.VK_D)
-        	camera.RotateClockwiseAboutUpAxis(1f);
-        else if(k.getKeyCode() == KeyEvent.VK_W)
-        	camera.RotateCounterClockwiseAboutRightAxis(1f);
-        else if(k.getKeyCode() == KeyEvent.VK_S)
-        	camera.RotateClockwiseAboutRightAxis(1f);
-    }
+	@Override
+	public void keyPressed(KeyEvent k) {
+		if (k.getKeyCode() == KeyEvent.VK_LEFT)
+			camera.MoveLeft(20.0f);
+		else if (k.getKeyCode() == KeyEvent.VK_RIGHT)
+			camera.MoveRight(20.0f);
+		else if (k.getKeyCode() == KeyEvent.VK_UP)
+			camera.MoveUp(20.0f);
+		else if (k.getKeyCode() == KeyEvent.VK_DOWN)
+			camera.MoveDown(20.0f);
+		else if (k.getKeyCode() == KeyEvent.VK_CONTROL)
+			camera.Print_Info();
+		else if (k.getKeyCode() == KeyEvent.VK_A)
+			camera.RotateCounterClockwiseAboutUpAxis(1f);
+		else if (k.getKeyCode() == KeyEvent.VK_D)
+			camera.RotateClockwiseAboutUpAxis(1f);
+		else if (k.getKeyCode() == KeyEvent.VK_W)
+			camera.RotateCounterClockwiseAboutRightAxis(1f);
+		else if (k.getKeyCode() == KeyEvent.VK_S)
+			camera.RotateClockwiseAboutRightAxis(1f);
+		else if (k.getKeyCode() == KeyEvent.VK_Q)
+			camera.MoveForward(20f);
+		else if (k.getKeyCode() == KeyEvent.VK_Z)
+			camera.MoveBackward(20f);
+	}
 
-    @Override
-    public void keyReleased(KeyEvent k) {
-        //System.out.println("Key released!");
-    }
+	@Override
+	public void keyReleased(KeyEvent k) {
+		// System.out.println("Key released!");
+	}
+
+	@Override
+	public void run() {
+		long lastTime = System.nanoTime();
+		long starttime = System.currentTimeMillis();
+		final double ns = 1000000000.0 / 60.0;// 60 times per second
+		double delta = 0;
+		boolean buffstrat = false;
+		requestFocus();
+		while (running) {
+			long now = System.nanoTime();
+			if (System.currentTimeMillis() - 1000 > starttime) {
+				fps = frames;
+				frames = 0;
+				starttime = System.currentTimeMillis();
+			}
+			delta = delta + ((now - lastTime) / ns);
+			lastTime = now;
+			/*
+			 * while (delta >= 1)//Make sure update is only happening 60 times a
+			 * second { //handles all of the logic restricted time
+			 * ApplyEvents(); delta--; }
+			 */
+			ApplyEvents();
+			if (buffstrat == false) {
+				if (Run.frame.getBufferStrategy() == null)
+					continue;
+				buffstrat = true;
+			}
+			render();// displays to the screen unrestricted time
+		}
+
+	}
 }
