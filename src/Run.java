5f3e0c69855543da3613bb33078ece879611f6b1
stockfishdeveloper <davidandjava@mail.com>
2017-03-21 09:46:57 -0500

    Change test code

--- src/Run.java
+++ src/Run.java
@@ -12,12 +12,12 @@ class Run
     public static void main(String[] args) throws InterruptedException, IOException
     {
     	cam = new Camera();
-    	World w = new World();
-        frame = new JFrame();
+    	frame = new JFrame();
         frame.setSize(1280, 1000);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel = new JPanel();
         panel.setLayout(null);
+        World w = new World();
         panel.add(w);
         panel.addMouseMotionListener(w);
         panel.addMouseWheelListener(w);
@@ -25,23 +25,28 @@ class Run
         panel.addKeyListener(w);
         frame.add(panel);
         frame.setVisible(true);
+        frame.createBufferStrategy(3);
         nextframe = System.currentTimeMillis();
-        //Point p = new Point(10, 0, 0);
+        /*Point p = new Point(1, 0, 0);
+        World.camera.RotateClockwiseAboutUpAxis(34);
+        World.camera.RotateClockwiseAboutRightAxis(12);
+        Point p2 = World.camera.RotMat.Multiply(World.camera.GetDirection());
+        p2.Print_Info();*/
         //p.RotateCounterClockwiseAboutYAxis(new Point(2, 0, 0), 180);
         //Point p1 = w.camera.LookAt(p);
         //p1.Print_Info();
-        while(true)
+        /*while(true)
         {
         	if(System.currentTimeMillis() >= nextframe)
             {
         		//w.camera.RotMat.Output();
-        		//w.cube.RotateClockwiseAboutYAxis(new Point(0, 0, 0), 1.5f);
-                //w.cube.RotateClockwiseAboutXAxis(new Point(0, 0, 0), 1.5f);
+        		w.cube.RotateClockwiseAboutYAxis(new Point(0, 0, 0), 1.5f);
+                w.cube2.RotateClockwiseAboutXAxis(new Point(0, 0, 0), 1.5f);
         		//World.camera.MoveBackward(50);
                 //System.out.println(Util.Distance_Between(World.camera.GetDirection(), new Point(0, 0, 0)));
         		w.repaint();
                 nextframe += 100;
             }
-        }
+        }*/
     }
 }
